0cf95e1872c43b7cebbfcda26536e15a97d81f00
smolinskiJP <jp.smolinski05@gmail.com>
2025-05-07 11:16:20 -0300

    MEIRA IS INTO DRUGS (I'm starting to understand)

--- include/universalsort.h
+++ include/universalsort.h
@@ -7,6 +7,8 @@ void quickSort3Ins(int * A, int l, int r);
 void partition3(int * A, int l, int r, int *i, int *j);
 void insertionSort(int v[], int l, int r);
 int median(int a, int b, int c);
-
+void universalSort(int * A, int tam, int minSizePartition, int breakMax);
+int countBreak(int * A, int tam);
+int definePartitionSize(int * A, int tam, int limiarCusto);
 
 #endif
